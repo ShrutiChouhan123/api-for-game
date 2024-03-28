11ace8fee4652a33d70640eaaa38a6efc1c53e04
shrutichouhan58 <shchouhan@bestpeers.in>
2024-03-28 18:55:52 +0530

    solved cors issue

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,8 +6,6 @@ class AuthController {
         return res.status(400).json({ message: 'email and password are required' });
       }
   
-      // In a real application, you would perform authentication here
-      // For demonstration, let's simply echo back the provided credentials
       console.log(email,password)
       res.status(200).json({ message: 'Login successful', email, password });
     }
--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-// server.js
 const express = require('express');
-const bodyParser = require('body-parser');
-const authController = require('./controllers/authController');
 const cors = require('cors');
+const authController = require('./controllers/authController');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Enable CORS for all origins
 app.use(cors());
-app.use(express.json())
+
+app.use(express.json());
 
 app.post('/login', authController.login);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-  });
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
